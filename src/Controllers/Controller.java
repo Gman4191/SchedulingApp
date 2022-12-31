4d3872bda4e1356c5228f81c3cb36832454e5935
gavin <gpulley@wgu.edu>
2022-12-31 14:04:55 -0700

    print out when initialized

--- src/Controllers/Controller.java
+++ src/Controllers/Controller.java
@@ -8,6 +8,6 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        System.out.println("Controller is initialized");
     }
 }
