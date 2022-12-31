1ebe89e651a592a1758c8f0d4cc63ac6f84e2dfd
gavin <gpulley@wgu.edu>
2022-12-31 15:10:14 -0700

    Database Connection classes added

---
+++ src/DAO/DBConnection.java
@@ -0,0 +1,31 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class DBConnection {
+    private static final String protocol = "jdbc";
+    private static final String userName = "sqlUser";
+    private static final String vendorName = ":mysql:";
+    private static final String serverIp = "//127.0.0.1:3306/";
+    private static final String password = "Passw0rd!";
+    private static final String databaseName = "client_schedule";
+    private static final String MYSQLJBCDRIVER = "com.mysql.cj.jdbc.Driver";
+
+    private static final String jdbcURL = protocol + vendorName + serverIp + databaseName;
+
+    private static Connection conn = null;
+    public static Connection makeConnection()
+    {
+        try
+        {
+            Class.forName(MYSQLJBCDRIVER);
+            conn = DriverManager.getConnection(jdbcURL, userName, password);
+            System.out.println("Connection made!");
+        } catch(Exception e)
+        {
+            System.out.println("Connection failed");
+        }
+        return conn;
+    }
+}
